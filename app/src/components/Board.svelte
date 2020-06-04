1ca5de025a5f886001f1b8b3b885c446e0b69ba2
Timo Blomqvist <blomqvist.timo@gmail.com>
2020-06-04 15:32:11 +0300

    Svelte boilerplate + some chess stuff

---
+++ app/src/components/Board.svelte
@@ -0,0 +1,22 @@
+<script>
+	import Square from './Square.svelte';
+	let files = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+	let ranks = new Array(8);
+</script>
+
+<div>
+    {#each ranks as r, i}
+        {#each files as f}
+            <Square file={ f } rank={ 8 - i } />
+        {/each}
+    {/each}
+</div>
+
+<style>
+	div {
+		width: 800px;
+		display: grid;
+		grid-template-columns: auto auto auto auto auto auto auto auto;
+		grid-gap: 0;
+	}
+</style>
---
+++ app/src/components/Square.svelte
@@ -0,0 +1,29 @@
+<script>
+    export let file;
+    export let rank;
+    let dark = ['a', 'c', 'e', 'g'].includes(file) ? rank % 2 === 0 : rank % 2 === 1;
+</script>
+
+<div class:dark>
+    <i class="fas fa-chess-queen"></i>
+    <span>
+        {`${file}${rank}`}
+    </span>
+</div>
+
+<style>
+    div {
+        background: #a87b37;
+        height: 100px;
+        width: 100px;
+        display: flex;
+        justify-content: flex-end;
+        align-items: flex-end;
+        color: #664808;
+        font-weight: 800;
+    }
+    div.dark {
+        background: #664808;
+        color: #a87b37;
+    }
+</style>
